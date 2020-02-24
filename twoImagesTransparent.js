8783a1d9b24aec4bc61f742454da6a7e187df332
KasperNiss <58505554+KasperNiss@users.noreply.github.com>
2020-02-24 13:29:58 +0100

    Add files via upload

---
+++ twoImagesTransparent.js
@@ -0,0 +1,46 @@
+let img;
+let img2;
+let input;
+
+function preload() {
+  img2 = loadImage("PF.jpg");
+}
+
+let slider;
+
+function setup() {
+  createCanvas(188,242);
+  input = createFileInput(handleFile);
+  input.position(200,0);
+  button = createButton('Save Image');
+  button.position(200,19);
+  button.mousePressed(SAVE);
+  image(img2, 0,0,188,242);
+  slider = createSlider(0, 1, 0.5, 0);
+slider.position(200, 40);
+slider.style('width', '100px');
+
+
+}
+
+function draw(){
+  image(img2, 0, 0, 188, 242);
+  if(img){
+    image(img, 0, 0, 188, 242);
+    tint(255, 255 * slider.value());
+  }
+
+}
+
+ function handleFile(file) {
+   if (file.type ==='image') {
+     img = loadImage(file.data,'');
+     img.hide();
+   } else {
+     img = null;
+   }
+ }
+
+  function SAVE () {
+    saveCanvas ('sjovtbilledexD','jpg');
+  }
